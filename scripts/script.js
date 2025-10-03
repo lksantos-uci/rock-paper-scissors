edfd0411b48ae2197b324e37c84b768e0c161718
lksantos <lksantos@uci.edu>
2025-10-02 17:43:04 -0700

    Finish rock paper scissors
    I implemented playRound() and playGame(), such that playGame() calls
    playRound() however many times defined with the NUM_ROUNDS constant.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
-
+const NUM_ROUNDS = 5;
 
 // This function decides between rock, paper, or scissors randomly.
 function getComputerChoice() {
@@ -30,13 +30,47 @@ function isValid(choice) {
 // This function allows the player to decide their move.
 function getHumanChoice() {
     // Prompt user for their answer until a valid choice is entered.
-    let choice = prompt("Enter your move: ");
+    let choice = prompt("Enter your move: ").toLowerCase();
     while (!isValid(choice)) {
-        choice = prompt("Try again. Only \"rock\", \"paper\" or \"scissors\" is accepted: ");
+        choice = prompt("Try again. Only \"rock\", \"paper\" or \"scissors\" is accepted: ").toLowerCase();
     }
     // Return their choice
     return choice;
 }
 
 
+// This function plays a single round of the game through.
+function playRound(humanChoice, computerChoice) {
+    // Case 1: Tie breaker
+    if (humanChoice === computerChoice) {
+        console.log(`It is a tie between ${humanChoice} and ${computerChoice}!`);
+    }
 
+    // Case 2: Player wins
+    else if ((humanChoice === "rock" && computerChoice === "scissors") ||
+             (humanChoice === "paper" && computerChoice === "rock") ||
+             (humanChoice === "scissors" && computerChoice === "paper")) {
+        console.log(`Player wins! The player's ${humanChoice} has defeated the computer's ${computerChoice}.`);
+        humanScore += 1;
+    }
+
+    // Case 3: The computer wins
+    else {
+        console.log(`Player loses...The player's ${humanChoice} was no match for the computer's ${computerChoice}.`);
+        computerScore += 1;
+    }
+}
+
+
+// This function plays through all rounds of the game.
+function playGame() {
+    // Play for however many rounds defined.
+    for (let index = 0; index < NUM_ROUNDS; index++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+}
+
+// Start the game
+playGame();
